5192c86da6fc11b36cd6f948a2bd24258776ade3
alexturpin <alex.turpin@gmail.com>
2018-10-10 20:01:15 -0400

    Prettier

--- tools/fetch.js
+++ tools/fetch.js
@@ -1,20 +1,20 @@
 const pako = require('pako');
 const request = require('request');
 const fs = require('fs');
- 
+
 const requestOptions = {
 	url: 'http://prod.escapefromtarkov.com/client/trading/api/getTradersList',
 	method: 'POST',
 	gzip: true,
 	encoding: null,
 	headers: {
-		'Accept': '*/*',
+		Accept: '*/*',
 		'Accept-Encoding': 'identity',
 		'User-Agent': 'UnityPlayer/5.6.6f2 (http://unity3d.com)',
-		'Cookie': 'PHPSESSID=',
+		Cookie: 'PHPSESSID=',
 		'X-Unity-Version': '5.6.6f2',
-		'Connection': 'Keep-Alive',
-		'Host': 'prod.escapefromtarkov.com'
+		Connection: 'Keep-Alive',
+		Host: 'prod.escapefromtarkov.com'
 	}
 };
 
@@ -24,7 +24,7 @@ request(requestOptions, (err, response, body) => {
 	}
 
 	const decompressed = pako.inflate(body, {
-		to: 'string',
+		to: 'string'
 	});
 
 	fs.writeFileSync('items.json', decompressed, 'utf-8');
